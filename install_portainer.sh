f8d014deec5cbddaa5403d551b303f61cf629d2e
vinayns <davidbeckham752003@gmail.com>
2023-08-07 19:31:56 +0200

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -4,16 +4,9 @@
 
 echo "Script to install Portainer"
 
-# Download YML file and isntall it
-echo "Download docker compose yml file for Portainer"
-mkdir -p docker
-cd docker
-echo "Downloading yml file"
-wget https://raw.githubusercontent.com/vinayns/homeLabSetup/main/docker-compose-portainer.yml
-echo "Renaming yml file"
-mv docker-compose-portainer.yml docker-compose.yml
-echo "YML file ready for install with Docker-Compose"
-
 echo "Installing Poratiner.."
-docker compose up -d
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
 echo "Portainer Installed!"
